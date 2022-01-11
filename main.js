6ffe6d0faaa85fd2a6097e79de2c15bc7935892c
emtroublemaker <83471925+emtroublemaker@users.noreply.github.com>
2022-01-11 17:54:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -603,23 +603,32 @@ function checkSecond(sec) {
   return sec;
 }
 
+// Timer
 
-   /* Timer */
-#timer{
-    border: 3px solid rgb(240, 236, 236);
-    /* padding-top: 50px; */
-    padding-right: 10px;
-    padding-bottom: 1px;
-    padding-left: 10px;
+document.getElementById('timer').innerHTML =
+  02 + ":" + 01;
+startTimer();
 
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  if(m<0){
+    return
   }
-  div{
-    background-color:black;
-    color: white;
-    font-size:30px;
-    text-align: center;
-    padding-top: 60px;
-    padding-right: 80px;
-    padding-bottom: 50px;
-    padding-left: 80px;
-  }
+  
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+  
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
